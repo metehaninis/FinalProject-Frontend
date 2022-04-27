c9b579a5197c3c89848c6b0a7d24a806487d473a
metehan iniş <metehan-330@hotmail.com>
2022-04-27 21:06:01 +0300

    Add project files.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -10,18 +12,48 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
-  dataLoaded=false;
-  
-  constructor(private productService:ProductService) {}
+  dataLoaded = false;
+  filterText = '';
+
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService,
+    private cartService:CartService
+  ) {}
 
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['categoryId']) {
+        this.getProductsByCategory(params['categoryId']);
+      } else {
+        this.getProducts();
+      }
+    });
   }
 
   getProducts() {
-    this.productService.getProducts().subscribe(response=>{
-      this.products=response.data
-      this.dataLoaded=true;
-    })
+    this.productService.getProducts().subscribe((response) => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    });
+  }
+
+  getProductsByCategory(categoryId: number) {
+    this.productService
+      .getProductsByCategory(categoryId)
+      .subscribe((response) => {
+        this.products = response.data;
+        this.dataLoaded = true;
+      });
+  }
+
+  addToCart(product: Product) {
+    if (product.productId === 1) {
+      this.toastrService.error('Hata', 'Bu ürün sepete eklenemez');
+    } else {
+      this.toastrService.success('Sepete eklendi', product.productName);
+      this.cartService.addToCart(product);
+    }
   }
 }
